939c3212bedbd4fa911aad16b98c5bab27b10f65
EwanAbdo <59457842+EwanAbdo@users.noreply.github.com>
2023-02-27 20:30:00 -0700

    Update app.py

--- app.py
+++ app.py
@@ -33,18 +33,10 @@ def run_script():
     city = get_city()
     path = upload_file()
     results = subprocess.run(['python', 'LinkedIn6.py',path,city], capture_output=True,text=True)#,stdout=subprocess.PIPE
-    if results.returncode == 0:
-        return 'Script executed successfully!'
-    else:
+    if results.returncode == -1:
         return 'Nothing was found! Try another search'
-
-@app.route('/post',methods=['POST','GET'])
-def post():
-    file=request
-    return file
-
-
-
+    else:
+        return 'Script executed successfully!'
 
 if __name__ == '__main__':
     app.run(debug=True)
